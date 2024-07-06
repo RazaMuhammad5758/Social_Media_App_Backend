906ec5342ec0972fdbd6a704c0afde0265fb3a72
RazaMuhammad5758 <waistband161@gmail.com>
2024-07-07 03:31:00 +0500

    api checking

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import userRouter from "./routes/users.js";
+const app = express();
+
+
+app.use("/api/users", userRouter);
+
+
+app.listen(8800, ()=>{
+
+    console.log("API working");
+})
+
