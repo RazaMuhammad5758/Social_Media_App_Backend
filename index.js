ef76c403baf6c36d58a60f96d41bef96c06c922f
RazaMuhammad5758 <waistband161@gmail.com>
2024-07-10 12:46:54 +0500

    api setting

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from "express";
 import userRouter from "./routes/users.js";
+import postRouter from "./routes/posts.js";
+import likeRouter from "./routes/likes.js";
+import commentRouter from "./routes/comments.js";
+import authRouter from "./routes/auth.js";
 const app = express();
 
 
 app.use("/api/users", userRouter);
+app.use("/api/users", postRouter);
+app.use("/api/users", likeRouter);
+app.use("/api/users", commentRouter);
+app.use("/api/users", authRouter);
+
 
 
 app.listen(8800, ()=>{
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,9 @@
 import express from "express"
+import { getUser } from "../controllers/user.js"
 
 const router = express.Router()
 
-router.get("/test", (req, res)=>{
-    res.send("it Workks")
-})
+router.get("/test", getUser)
 
 export default router
 
