a2be315bd02dc64e89124e1eeacaf4a64d883bd6
RazaMuhammad5758 <waistband161@gmail.com>
2024-07-11 20:41:45 +0500

    changes

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
+const app = express();
 import userRouter from "./routes/users.js";
 import postRouter from "./routes/posts.js";
 import likeRouter from "./routes/likes.js";
 import commentRouter from "./routes/comments.js";
 import authRouter from "./routes/auth.js";
-const app = express();
+
+//middlewares
+
+app.use(express.json())
 
 
 app.use("/api/users", userRouter);
@@ -15,8 +19,8 @@ app.use("/api/auth", authRouter);
 
 
 
-app.listen(8800, ()=>{
+const PORT = 8800; // Change this to a different port number if needed
 
-    console.log("API is working");
-})
-
+app.listen(PORT, () => {
+    console.log(`API is working on port ${PORT}`);
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 import express from "express"
-import { } from "../controllers/auth.js"
+import { login, logout, register } from "../controllers/auth.js"
 
 const router = express.Router()
 
-router.get("",)
+router.post("/register", register)
+router.post("/login", login)
+router.post("/logout", logout)
 
 export default router
 
